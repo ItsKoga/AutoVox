242bd45ed465d42c8bd02f616a37bc2a54f09651
ItsKoga <llouis2202@gmail.com>
2024-09-06 18:47:21 +0200

    feat: Add StandardRoles cog for managing standard roles in the guild

--- cogs/standard_roles.py
+++ cogs/standard_roles.py
@@ -12,8 +12,7 @@ import log_helper
 
 import database
 
-log = log_helper.create("StandardRoles")
-
+logger = log_helper.Logger("StandardRoles")
 
 class StandardRoles(commands.Cog):
     def __init__(self, bot):
@@ -23,17 +22,17 @@ class StandardRoles(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member):
         # When a member joins the guild
-        log(f"Member joined: {member.name}({member.id}) on {member.guild.name}({member.guild.id})", log_helper.LogTypes.INFO)
+        logger.log(f"Member joined: {member.name}({member.id}) on {member.guild.name}({member.guild.id})", log_helper.LogTypes.INFO)
         # Get the guild's standard roles
         standardRoles = database.execute_read_query(f"SELECT * FROM standard_roles WHERE guild_id = {member.guild.id}")
         # load all standard roles for the guild
-        standardRoles = [role[0] for role in standardRoles]
+        standardRoles = [role[1] for role in standardRoles]
         guild = member.guild
         for role in guild.roles:
             if role.id in standardRoles:
                 await member.add_roles(role)
-                log(f"Added role {role.name}({role.id}) to {member.name}({member.id})", log_helper.LogTypes.INFO)
-        log(f"Added standard roles to {member.name}({member.id})", log_helper.LogTypes.INFO)
+                logger.log(f"Added role {role.name}({role.id}) to {member.name}({member.id})", log_helper.LogTypes.INFO)
+        logger.log(f"Added standard roles to {member.name}({member.id})", log_helper.LogTypes.INFO)
 
 
 def setup(bot):
