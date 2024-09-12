8ffd4ff9f44f0b4ebde24a18887ea957aa1aae5e
ItsKoga <llouis2202@gmail.com>
2024-09-12 08:39:11 +0200

    Refactor advertise functionality and add invite and support commands

---
+++ cogs/advertise.py
@@ -0,0 +1,56 @@
+import discord
+from discord.ext import commands, tasks
+from discord.commands import slash_command, Option, SlashCommandGroup
+from discord import InteractionContextType as ict
+
+import time as tm
+import asyncio
+
+import os
+
+import log_helper
+
+import translation
+
+import config
+
+import database
+
+logger = log_helper.Logger("Advertise")
+
+
+class Advertise(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @slash_command(name="invite", description="Get the invite link for the bot")
+    async def invite(self, ctx):
+        invite_link = config.load_value("invite_link")
+
+        embed = discord.Embed(title=translation.get(ctx.user.id, "invite_title"), description=translation.get(ctx.user.id, "invite_description").format(invite_link=invite_link), color=0x00ff00)
+        embed.set_footer("Made with ❤ by the AutoVox team")
+
+        await ctx.response.send_message(embed=embed)
+
+
+    @slash_command(name="support", description="Get the support server link for the bot")
+    async def support(self, ctx):
+        support_link = config.load_value("support_link")
+
+        embed = discord.Embed(title=translation.get(ctx.user.id, "support_title"), description=translation.get(ctx.user.id, "support_description").format(discord_link=support_link), color=0x00ff00)
+        embed.set_footer("Made with ❤ by the AutoVox team")
+
+        await ctx.response.send_message(embed=embed)
+
+    @slash_command(name="server", description="Get the server link for the bot")
+    async def server(self, ctx):
+        support_link = config.load_value("support_link")
+
+        embed = discord.Embed(title=translation.get(ctx.user.id, "support_title"), description=translation.get(ctx.user.id, "support_title").format(discord_link=support_link), color=0x00ff00)
+        embed.set_footer("Made with ❤ by the AutoVox team")
+
+        await ctx.response.send_message(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Advertise(bot))
